ca36a6898d152ee1b1fb68148340d0dcbc406c06
keremyy <marijn@netje.nl>
2025-10-03 11:08:53 +0200

    errors gefixt

--- Main.hs
+++ Main.hs
@@ -1,17 +1,16 @@
-import Data.Char (isSpace)
 
 main :: IO ()
 main = do
     putStrLn "Geef een bestand"
     name <- getLine
     contents <- readFile name
-    let outputName = repleceHTML ".html" ".md" name
+    let outputName = replaceHTML ".html" ".md" name
     writeFile outputName (removeTussenLijn (toLeft (tags (htmlToMarkdown contents))))
 
 toLeft :: String -> String
-toLeft = unlines . map (dropWhile myIsSpace) . lines
-myIsSpace :: Char -> Bool
-myIsSpace c = c == ' ' || c == '\t' || c == '\n'
+toLeft = unlines . map (dropWhile geenSpatie) . lines
+geenSpatie :: Char -> Bool
+geenSpatie c = c == ' ' || c == '\n'
 
 
 tags :: String -> String
@@ -22,46 +21,74 @@ tags = go
     go (x:xs) = x : go xs
 
 removeTussenLijn :: String -> String
-removeTussenLijn = unlines . filter (not . all isSpace) . lines
+removeTussenLijn = unlines . filter (not . all geenSpatie) . lines
 
 htmlToMarkdown :: String -> String
-htmlToMarkdown = repleceHTML "</li>" "\n"
-              . repleceHTML "</ol>" "\n"
-              . repleceHTML "<li>" "- "
-              . repleceHTML "<ol>" "\n"
-              . repleceHTML "</ul>" "\n"
-              . repleceHTML "<ul>" "\n"
-              . repleceHTML "<br/>" "\n"
-              . repleceHTML "<br>" "\n"
-              . repleceHTML "</p>" "\n\n"
-              . repleceHTML "<p>" ""
-              . repleceHTML "</h3>" "\n"
-              . repleceHTML "<h3>" "### "
-              . repleceHTML "</h2>" "\n"
-              . repleceHTML "<h2>" "## "
-              . repleceHTML "</h1>" "\n"
-              . repleceHTML "<h1>" "# "
-              . repleceHTML "</em>" "_"
-              . repleceHTML "<em>" "_"
-              . repleceHTML "</i>" "_"
-              . repleceHTML "<i>" "_"
-              . repleceHTML "</strong>" "**"
-              . repleceHTML "<strong>" "**"
-              . repleceHTML "</b>" "**"
-              . repleceHTML "<b>" "**"
+htmlToMarkdown =
+      replaceHTML "<h1>" "# "
+    . replaceHTML "</h1>" "\n\n"
+    . replaceHTML "<h2>" "## "
+    . replaceHTML "</h2>" "\n\n"
+    . replaceHTML "<h3>" "### "
+    . replaceHTML "</h3>" "\n\n"
+    . replaceHTML "<h4>" "#### "
+    . replaceHTML "</h4>" "\n\n"
+    . replaceHTML "<h5>" "##### "
+    . replaceHTML "</h5>" "\n\n"
+    . replaceHTML "<h6>" "###### "
+    . replaceHTML "</h6>" "\n\n"
 
-repleceHTML :: String -> String -> String -> String
-repleceHTML old new str = go str
+    -- Tekststijl
+    . replaceHTML "<b>" "**"
+    . replaceHTML "</b>" "**"
+    . replaceHTML "<strong>" "**"
+    . replaceHTML "</strong>" "**"
+    . replaceHTML "<i>" "_"
+    . replaceHTML "</i>" "_"
+    . replaceHTML "<em>" "_"
+    . replaceHTML "</em>" "_"
+    . replaceHTML "<code>" "`"
+    . replaceHTML "</code>" "`"
+    . replaceHTML "<pre>" "```\n"
+    . replaceHTML "</pre>" "\n```"
+
+    -- Lijsten
+    . replaceHTML "<ul>" "\n"
+    . replaceHTML "</ul>" "\n"
+    . replaceHTML "<ol>" "\n"
+    . replaceHTML "</ol>" "\n"
+    . replaceHTML "<li>" "- "
+    . replaceHTML "</li>" "\n"
+
+    -- Links & afbeeldingen
+    . replaceHTML "<a href=\"" "["
+    . replaceHTML "\">" "]("
+    . replaceHTML "</a>" ")"
+    . replaceHTML "<img src=\"" "!["
+    . replaceHTML "\" alt=\"" "]("
+    . replaceHTML "\">" ")"
+
+    -- Regels en paragrafen
+    . replaceHTML "<br>" "  \n"
+    . replaceHTML "<br/>" "  \n"
+    . replaceHTML "<p>" "\n\n"
+    . replaceHTML "</p>" "\n\n"
+    . replaceHTML "<hr>" "\n---\n"
+    . replaceHTML "<hr/>" "\n---\n"
+
+
+replaceHTML :: String -> String -> String -> String
+replaceHTML old new str = go str
   where
     go [] = []
     go xs@(x:rest)
-      | old `isPrefixOf` xs = new ++ go (drop (length old) xs)
+      | old `isPrefixVan` xs = new ++ go (drop (length old) xs)
       | otherwise = x : go rest
 
-isPrefixOf :: Eq a => [a] -> [a] -> Bool
-isPrefixOf [] _ = True
-isPrefixOf _ [] = False
-isPrefixOf (x:xs) (y:ys) = x == y && isPrefixOf xs ys
+isPrefixVan :: Eq a => [a] -> [a] -> Bool
+isPrefixVan [] _ = True
+isPrefixVan _ [] = False
+isPrefixVan (x:xs) (y:ys) = x == y && isPrefixVan xs ys
 
 
 
